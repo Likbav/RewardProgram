7a4becaefd46260a164d403385a48e48c5106c04
Likbav <117484379+Likbav@users.noreply.github.com>
2023-01-15 13:20:48 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # RewardProgram
+
+Here are the instructions for succesfully running this project
+
+1) Task1.zip have the code for REACT frontend pages
+2) Extract this folder and run it from VS Code
+
+
+3) Extract RewardProgram folder that has the WEB API 
+
+4) scripts.sql file have the database scripts once executed on SQL Server, it creates the schema in database needed for running the project
+
---
+++ scripts.sql
@@ -0,0 +1,41 @@
+create database rewardprogram
+
+use rewardprogram
+
+create table transactions(month varchar(50),vendorname varchar(50),transactiondate datetime,amount float)
+
+
+insert into transactions values('january','AT&T','1/14/2022 14:00',20.5)
+insert into transactions values('january','Amazon','1/15/2022 14:00',57.5)
+insert into transactions values('january','BestBuy','1/16/2022 14:00',120)
+insert into transactions values('january','Costco','1/17/2022 14:00',90.5)
+insert into transactions values('january','Walmart','1/18/2022 14:00',240.5)
+insert into transactions values('january','Liqor Mart','1/19/2022 14:00',1120.5)
+insert into transactions values('january','DQ','1/20/2022 14:00',10.5)
+insert into transactions values('january','Wendys','1/21/2022 14:00',2)
+insert into transactions values('january','U-Haul','1/22/2022 14:00',100)
+insert into transactions values('january','DollarTree','1/23/2022 14:00',50)
+
+
+insert into transactions values('february','AT&T','2/14/2022 14:00',8.5)
+insert into transactions values('february','Amazon','2/15/2022 14:00',7.5)
+insert into transactions values('february','BestBuy','2/16/2022 14:00',200)
+insert into transactions values('february','Costco','2/17/2022 14:00',901)
+insert into transactions values('february','Walmart','2/18/2022 14:00',70)
+insert into transactions values('february','Liqor Mart','2/19/2022 14:00',15)
+insert into transactions values('february','DQ','2/20/2022 14:00',105)
+insert into transactions values('february','Wendys','2/21/2022 14:00',21)
+insert into transactions values('february','U-Haul','2/22/2022 14:00',1003)
+insert into transactions values('february','DollarTree','2/23/2022 14:00',510)
+
+insert into transactions values('march','AT&T','3/14/2022 14:00',26.5)
+insert into transactions values('march','Amazon','3/15/2022 14:00',515)
+insert into transactions values('march','BestBuy','3/16/2022 14:00',70)
+insert into transactions values('march','Costco','3/17/2022 14:00',50)
+insert into transactions values('march','Walmart','3/18/2022 14:00',1140)
+insert into transactions values('march','Liqor Mart','3/19/2022 14:00',120.5)
+insert into transactions values('march','DQ','3/20/2022 14:00',105)
+insert into transactions values('march','Wendys','3/21/2022 14:00',212)
+insert into transactions values('march','U-Haul','3/22/2022 14:00',101)
+insert into transactions values('march','DollarTree','3/23/2022 14:00',540)
+
